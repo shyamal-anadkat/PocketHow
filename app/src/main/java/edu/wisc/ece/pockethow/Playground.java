3914d66a883a5069f96dc2bfd7fa9fd14e922cb3
Shyamal Anadkat <shyamalanadkat@gmail.com>
2017-10-15 16:59:37 -0500

    init project

---
+++ app/src/main/java/edu/wisc/ece/pockethow/Playground.java
@@ -0,0 +1,14 @@
+package edu.wisc.ece.pockethow;
+
+/**
+ * Created by zosta on 10/15/2017.
+ */
+
+public class Playground {
+
+    public static void main(String [] args) {
+
+        System.out.print("Hello");
+
+    }
+}
