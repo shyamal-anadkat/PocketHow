ca0865d4c184ce061e76223a44e2de6f9fbb0ba6
LeePing1994 <onglp5@gmail.com>
2017-11-29 17:47:08 -0600

    Write database to Files
    We can now write the database to the "Files". Email the database to yourself to access it from your computer. These databases will be used in the server

--- app/src/main/java/edu/wisc/ece/pockethow/frontend/searchActivity.java
+++ app/src/main/java/edu/wisc/ece/pockethow/frontend/searchActivity.java
@@ -1,8 +1,14 @@
 package edu.wisc.ece.pockethow.frontend;
 
+import android.Manifest;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,10 +18,21 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-
+import static java.nio.file.StandardCopyOption.*;
 import edu.wisc.ece.pockethow.R;
 import edu.wisc.ece.pockethow.dbHandler.PHDBHandler;
 import edu.wisc.ece.pockethow.dbOperations.DbOperations;
@@ -37,7 +54,8 @@ public class searchActivity extends AppCompatActivity {
     ArrayList<String> categoryArrayList = new ArrayList<>();
     ArrayList<String> downloadedFilePathList = new ArrayList<>();
     final DbOperations dbOperations = new DbOperations(this);
-
+    private int requestCode;
+    private int grantResults[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -142,9 +160,91 @@ public class searchActivity extends AppCompatActivity {
         }
         //deleteDatabase("PocketHow.db");
         populateDB();
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},requestCode);
 
+
+        onRequestPermissionsResult(requestCode,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},grantResults);
+
+
+        /*
+        temporary for sending databases
+         */
+        Button writeButton = (Button) findViewById(R.id.writeButton);
+        writeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //sendEmail();
+                //checkExternalMedia();
+                //writeToSDFile();
+                File root = android.os.Environment.getExternalStorageDirectory();
+                //File dir = new File (root.getAbsolutePath() + "/download");
+                File dir = new File(root.getAbsoluteFile() + "/pockethowdatabases");
+                dir.mkdirs();
+                File file = new File(dir, "myData.txt");
+
+                try {
+
+                    file.createNewFile();
+                    FileOutputStream f = new FileOutputStream(file);
+                    PrintWriter pw = new PrintWriter(f);
+                    pw.println("Hi , How are you");
+                    pw.println("Hello");
+                    pw.flush();
+                    pw.close();
+                    f.close();
+                    File dest = new File(dir, "database.db");
+                    File source = new File(dbOperations.getDatabase().getPath());
+                    InputStream is = null;
+                    OutputStream os = null;
+                    try {
+                        is = new FileInputStream(source);
+                        os = new FileOutputStream(dest);
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = is.read(buffer)) > 0) {
+                            os.write(buffer, 0, length);
+                        }
+                    } finally {
+                        is.close();
+                        os.close();
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Log.i("file", "******* File not found. Did you" +
+                            " add a WRITE_EXTERNAL_STORAGE permission to the   manifest?");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+    @Override // android recommended class to handle permissions
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
 
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.d("permission", "granted");
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.uujm
+                    Toast.makeText(searchActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+
+                    //app cannot function without this permission for now so close it...
+                    onDestroy();
+                }
+                return;
+            }
+
+            // other 'case' line to check fosr other
+            // permissions this app might request
+        }
+    }
     public void populateDB() {
         //deleteDatabase("PocketHow.db");
         dbOperations.searchWordList.clear();
@@ -215,6 +315,12 @@ public class searchActivity extends AppCompatActivity {
                         downloadedFilePathList.clear();
                     }
                 }
+                //exporting database to Downloads folder
+                File downloadsPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                ///data/user/0/edu.wisc.ece.pockethow/databases/PocketHow.db
+                File sauron = new File(downloadsPath.getAbsoluteFile() + "/sauron.txt");
+                //File sauron = new File(Environment.getExternalStorageDirectory().toString() + "/sauron.txt");
+
             }
         }).start();
     }
